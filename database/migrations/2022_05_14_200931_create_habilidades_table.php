3981f6ae1c9f9cfcd98aa59527db48610fa9db2b
JuanMedinaC24 <juan.medina.m@tecsup.edu.pe>
2022-05-14 15:44:51 -0500

    lab09 Juan Medina

---
+++ database/migrations/2022_05_14_200931_create_habilidades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHabilidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('habilidades', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre');
+            $table->string('imagen');
+            $table->string('descripcion');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('habilidades');
+    }
+}
